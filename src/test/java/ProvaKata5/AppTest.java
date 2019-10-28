52e8659063ae0eee458d5577ae7d00277d27d06c
MarcoPiazzon <piazzon83@gmail.com>
2019-10-28 16:42:04 +0100

    Initial commit

---
+++ src/test/java/ProvaKata5/AppTest.java
@@ -0,0 +1,49 @@
+package ProvaKata5;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void Test(){
+        assertEquals(App.maxXP(1, 12),500);
+    }
+
+    @Test
+    public void Test1(){
+        assertEquals(App.maxXP(13, 144),6500);
+    }
+
+    @Test
+    public void Test2(){
+        assertEquals(App.maxXP(10, 63),3000);
+    }
+
+    @Test
+    public void Test3(){
+        assertEquals(App.maxXP(1, 63),500);
+    }
+
+    @Test
+    public void Test4(){
+        assertEquals(App.maxXP(63, 1),2500);
+    }
+
+    @Test
+    public void Test5(){
+        assertEquals(App.maxXP(0, 0),0);
+    }
+
+
+
+
+}
